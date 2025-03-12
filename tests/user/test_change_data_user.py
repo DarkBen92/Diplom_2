a5b4537f5d712421a5ddf74bd21d69db838d2b7e
Daniil Khayrullin <daniil.khayrullin@myoffice.team>
2025-03-13 00:48:50 +0300

    refactor test

--- tests/user/test_change_data_user.py
+++ tests/user/test_change_data_user.py
@@ -3,6 +3,7 @@ import allure
 from methods.auth_methods import AuthMethods
 from precondition.user_precondition import setup_authorization_user, setup_data_user
 from conftest import create_and_token_user
+from steps import user_steps
 
 
 class TestChangeDataUser:
@@ -14,7 +15,11 @@ class TestChangeDataUser:
             headers=create_and_token_user[0],
             params=setup_authorization_user(new_data)
         )
-        assert response.status_code == 200 and response.json()["success"] is True
+        user_steps.check_status_code(
+            expected_status_code=200,
+            actual_status_code=response.status_code
+        )
+        # assert response.json()["success"] is True
 
     @allure.title("Попытка изменение данных неавторизованного пользователя.")
     def test_change_data_user_not_authorization(self):
