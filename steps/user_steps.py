ef78db0caccadc2c7a82c8de7be1aa19c5e6908a
Daniil Khayrullin <daniil.khayrullin@myoffice.team>
2025-03-13 00:52:13 +0300

    refactor test

--- steps/user_steps.py
+++ steps/user_steps.py
@@ -1,7 +1,7 @@
 import allure
 
 
-@allure.step("Проверка статус кода и ответа")
+@allure.step("Проверка статус кода и ответа.")
 def check_status_code(
         expected_status_code: int,
         actual_status_code: int,
